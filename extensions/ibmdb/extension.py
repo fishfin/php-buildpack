593d9802a01ad5169fdf01a9e2464ddb3fa2ed82
abhradke <abhradke@in.ibm.com>
2017-03-01 11:44:04 +0530

    Update extension.py

--- extensions/ibmdb/extension.py
+++ extensions/ibmdb/extension.py
@@ -139,6 +139,7 @@ class IBMDBInstaller(ExtensionHelper):
         self._logMsg ('Abhinav filename:' + fileName)
         self._runCmd(os.environ, self._ctx['BUILD_DIR'], ['rm', '-rf', fileToInstall])
         self._log.debug("Installing direct [%s]", url)
+        self._logMsg ('Calling download for url = ' + url )
         self._installer._dwn.custom_extension_download(url, url, fileToInstall)
         self._logMsg ('Abhinav filetoinstall & install dir and url :' + fileToInstall + " " + installDir +"   " +url )
         #self._runCmd(os.environ, self._ctx['BUILD_DIR'], ['mv', fileToInstall, fileToInstall + 'tar.gz'])
