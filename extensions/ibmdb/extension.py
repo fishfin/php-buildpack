152b53f8549dbe861b6fb9c6dfe3930b230f5615
abhradke <abhradke@in.ibm.com>
2017-03-02 22:49:07 +0530

    Update extension.py

--- extensions/ibmdb/extension.py
+++ extensions/ibmdb/extension.py
@@ -232,7 +232,7 @@ class IBMDBInstaller(ExtensionHelper):
            #              self._phpExtnDir])
             
             #self._runCmd(os.environ, self._ctx['BUILD_DIR'], ['ls','-l',extnDownloadDir])
-        ''' self._logMsg ('extn dwnld dir = ' + extnDownloadDir)
+            self._logMsg ('extn dwnld dir = ' + extnDownloadDir)
             curdir = os.getcwd()
             #subprocess.call(['ls', '-l', extnDownloadDir])
             self._logMsg ('curdir = ' + curdir)
@@ -244,13 +244,13 @@ class IBMDBInstaller(ExtensionHelper):
             self._logMsg ('new dir ls')
             subprocess.call(['ls', '-lrt', newdir ])
             #self._runCmd(os.environ, self._ctx['BUILD_DIR'],['/tmp/app/php/bin/php','-i'])
-            self._runCmd(os.environ, self._ctx['BUILD_DIR'],['/tmp/app/php/bin/phpize'])
+        '''    self._runCmd(os.environ, self._ctx['BUILD_DIR'],['/tmp/app/php/bin/phpize'])
             self._runCmd(os.environ, self._ctx['BUILD_DIR'],['./configure -with-IBM_DB2='+ self._ctx['IBMDBCLIDRIVER_INSTALL_DIR']] )
             self._runCmd(os.environ, self._ctx['BUILD_DIR'],['make','clean'])
             self._runCmd(os.environ, self._ctx['BUILD_DIR'],['make'])
-            self._runCmd(os.environ, self._ctx['BUILD_DIR'],['make','install'])
+            self._runCmd(os.environ, self._ctx['BUILD_DIR'],['make','install'])'''
             os.chdir(curdir)
-            subprocess.call(['pwd'])'''
+            subprocess.call(['pwd'])
             
             self._logMsg ('Installed ' + ibmdbExtn + ' Extension to ' + self._phpExtnDir)
 
