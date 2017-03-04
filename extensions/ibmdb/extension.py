5768e4793cbf9e34489b0ad9e56474a1178adf67
abhradke <abhradke@in.ibm.com>
2017-03-04 12:20:35 +0530

    Update extension.py

--- extensions/ibmdb/extension.py
+++ extensions/ibmdb/extension.py
@@ -270,6 +270,7 @@ class IBMDBInstaller(ExtensionHelper):
             
             self._runCmd(os.environ,self._ctx['BUILD_DIR'], ['phpize'])
             #time.sleep(5)
+            subprocess.call(['ls', '-l'])
             self._runCmd(os.environ, self._ctx['BUILD_DIR'],['./configure -with-IBM_DB2='+ self._ctx['IBMDBCLIDRIVER_INSTALL_DIR']] )
             self._runCmd(os.environ, self._ctx['BUILD_DIR'],['make'])
             self._runCmd(os.environ, self._ctx['BUILD_DIR'],['make','install'])
