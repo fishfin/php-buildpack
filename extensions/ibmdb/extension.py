a76488fdf15cecd566373d862d6fc68f3c273516
abhradke <abhradke@in.ibm.com>
2017-03-06 18:22:50 +0530

    Update extension.py

--- extensions/ibmdb/extension.py
+++ extensions/ibmdb/extension.py
@@ -251,18 +251,18 @@ class IBMDBInstaller(ExtensionHelper):
             self._logMsg('tmpdir = '+  tmpdir)
             os.chdir(extnDownloadDir)
             newdir = os.getcwd()
-            self._logMsg ('New directory is : ' + curdir)
+            self._logMsg ('New directory is : ' + newdir)
             osev['PATH'] = phpBinDir +':' + os.environ['PATH']
             self._logMsg('osev path = ' + osev['PATH'])
             self._logMsg ('ls -l for new Directory')
             subprocess.call(['ls', '-l'])
 
             
-            self._logMsg('Phpize execute')
+            self._logMsg('Execute pecl')
             #self._runCmd(osev,self._ctx['BUILD_DIR'], ['php', '-i'])
             #self._runCmd(ose,self._ctx['BUILD_DIR'], ['phpize'])
             self._runCmd(osev,phpRoot, ['pecl','install','ibm_db2'])
-            self._runCmd(osev, phpBinDir,['./configure -with-IBM_DB2='+ self._ctx['IBMDBCLIDRIVER_INSTALL_DIR']] )
+            #self._runCmd(osev, phpBinDir,['./configure -with-IBM_DB2='+ self._ctx['IBMDBCLIDRIVER_INSTALL_DIR']] )
             self._runCmd(osev, extnDownloadDir,['make'])
             self._runCmd(osev, self._ctx['BUILD_DIR'],['make','install'])
 
