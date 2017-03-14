c5b28cc5110e324eed725bfa931eb3a39accef92
abhradke <abhradke@in.ibm.com>
2017-03-14 15:47:20 +0530

    Update extension.py

--- extensions/ibmdb/extension.py
+++ extensions/ibmdb/extension.py
@@ -1,56 +1,21 @@
-import json
 import logging
 import os
-import re
 import StringIO
-import subprocess
-import sys
-import time
-import tarfile
-import zipfile
 from urlparse import urlparse
 from build_pack_utils import stream_output
 from build_pack_utils import utils
 from extension_helpers import ExtensionHelper
 
-#from build_pack_utils.downloads import Downloader
-#from build_pack_utils.zips import UnzipUtil
-
-#_log = logging.getLogger(os.path.basename(os.path.dirname(__file__)))
-
-# Lets say there are two extensions e1 and e2 each with following methods used by buildpack
-#   configure; compile; service_environment; service_commands; preprocess_commands
-# The order in which these are called is as follows:
-#  1) e1.configure
-#  2) e2.configure
-#  3) Rewrite httpd.conf
-#  4) Extract and install httpd
-#  5) Rewrite php.ini
-#  6) Extract and install php
-#  7) e1.compile
-#  8) e2.compile
-#  9) e1.service_environment
-# 10) e2.service_environment
-# 11) e1.service_commands
-# 10) e2.service_commands
-# 13) e1.preprocess_commands
-# 14) e2.preprocess_commands
-
-CONSTANTS = {
-    'PHP_ARCH': '64',           # if not 64, 32-bit is assumed
-    'PHP_THREAD_SAFETY': 'nts', # if not ts, nts is assumed, case-insensitive
-}
-
 PKGDOWNLOADS =  {
+    #CLI_DRIVER
      'IBMDBCLIDRIVER1_DLFILE': 'linuxx64_odbc_cli.tar.gz',
      'IBMDBCLIDRIVER1_DLURL': 'https://public.dhe.ibm.com/ibmdl/export/pub/software/data/db2/drivers/odbc_cli/{IBMDBCLIDRIVER1_DLFILE}',
      
-    #IBM_DB Packages
-     'IBM_DB2_REPOSITORY': 'https://pecl.php.net/get',
-     'IBM_DB2_DLFILE': 'ibm_db2',
-     'IBM_DB2_DLURL': '{IBM_DB2_REPOSITORY}/{IBM_DB2_DLFILE}',     
-     'INCLUDE_FILE' : 'include.tar',
-     'INCLUDE_DLURL' : 'https://github.com/abhradke/include_php/blob/master/{INCLUDE_FILE}',
+    #IBM_DB Packages  
+     'IBM_DB_FILE' : 'ibm_db.tar.gz',
+     'IBM_DB_DLURL' : 'https://github.com/abhradke/include_php/blob/master/{IBM_DB_FILE}',
+     #'IBM_PDO_FILE' : 'ibm_db.tar.gz',
+     #'IBM_PDO_DLURL' : 'https://github.com/abhradke/include_php/blob/master/{IBM_PDO_FILE}',
 }
 
 class IBMDBInstaller(ExtensionHelper):
@@ -59,20 +24,26 @@ class IBMDBInstaller(ExtensionHelper):
         self._log = logging.getLogger(os.path.basename(os.path.dirname(__file__)))
 
         ExtensionHelper.__init__(self, ctx)
-        self._log.info('Detected PHP Version ' + self._ctx['PHP_VERSION'])
-        self._log.info('Using build pack directory ' + self._ctx['BP_DIR'])
-        self._log.info('Using build directory ' + self._ctx['BUILD_DIR'])
+        self._compilationEnv = os.environ
+        if 'IBM_DB_HOME' not in self._compilationEnv:
+            self._compilationEnv['IBM_DB_HOME'] = ''
+        if 'LD_LIBRARY_PATH' not in self._compilationEnv:
+            self._compilationEnv['LD_LIBRARY_PATH'] = ''
+        if 'PATH' not in self._compilationEnv:
+            self._compilationEnv['PATH'] = ''
 
         self._ibmdbClidriverBaseDir = 'ibmdb_clidriver'
-        self._phpRoot = os.path.join(self._ctx['BUILD_DIR'], 'php')
-        self._phpIniPath = os.path.join(self._phpRoot, 'etc', 'php.ini')
-        self._phpExtnDir = os.path.join(self._phpRoot, 'lib', 'php', 'extensions')
-
-        self._ctx['IBMDBCLIDRIVER_INSTALL_DIR'] = os.path.join(self._ctx['BUILD_DIR'], self._ibmdbClidriverBaseDir)
+        self._phpBuildRootDpath = os.path.join(self._ctx['BUILD_DIR'], 'php')
+        self._phpBuildIniFpath = os.path.join(self._phpBuildRootDpath, 'etc', 'php.ini')
 
     def _defaults(self):
         pkgdownloads = PKGDOWNLOADS
-        pkgdownloads['DOWNLOAD_DIR'] = os.path.join(self._ctx['BUILD_DIR'], '.downloads')        
+        pkgdownloads['COMPILATION_DIR'] = os.path.join(self._ctx['BUILD_DIR'], '.build_ibmdb_extension')
+        pkgdownloads['DOWNLOAD_DIR'] = os.path.join('{COMPILATION_DIR}', '.downloads')        
+        pkgdownloads['IBMDBCLIDRIVER_INSTALL_DIR'] = os.path.join(self._ctx['BUILD_DIR'], 'ibmdb_clidriver')
+        pkgdownloads['PHPSOURCE_INSTALL_DIR'] = os.path.join('{COMPILATION_DIR}', 'php')
+        pkgdownloads['IBM_DB2_DLDIR'] = os.path.join('{PHPSOURCE_INSTALL_DIR}', 'ext', 'ibm_db2')
+        pkgdownloads['PDO_IBM_DLDIR'] = os.path.join('{PHPSOURCE_INSTALL_DIR}', 'ext', 'pdo_ibm')
         return utils.FormattedDict(pkgdownloads)
 
     def _should_configure(self):
@@ -89,83 +60,35 @@ class IBMDBInstaller(ExtensionHelper):
         self._log.info(__file__ + "->compile")
         self._installer = install._installer
 
-        extnBaseDir = self._findPhpExtnBaseDir()
-        self._zendModuleApiNo = extnBaseDir[len(extnBaseDir)-8:]
-        self._phpExtnDir = os.path.join(self._phpExtnDir, extnBaseDir)
-        #self._phpApi, self._phpZts = self._parsePhpApi()
+        self._phpExtnDir = self.findPhpExtnBaseDir()
+        self._zendModuleApiNo = self._phpExtnDir[len(self._phpExtnDir)-8:]
+        self._phpExtnDpath = os.path.join(self._phpBuildRootDpath, 'lib', 'php', 'extensions', self._phpExtnDir)
+
         self.install_clidriver()
-        self.install_extensions()
+        self.download_extensions()
+        self.modifyPhpIni()
         self.cleanup()
         return 0
 
     def _service_environment(self):
         self._log.info(__file__ + "->service_environment")
         env = {
-            'IBM_DB_HOME': '$IBM_DB_HOME:$HOME/' + self._ibmdbClidriverBaseDir + '/lib',
-            'LD_LIBRARY_PATH': self._phpRoot + '/lib:' + '$LD_LIBRARY_PATH:$HOME/' + self._ibmdbClidriverBaseDir + '/lib',
-            'DB2_CLI_DRIVER_INSTALL_PATH': '$HOME/' + self._ibmdbClidriverBaseDir,
-            'PATH': self._phpRoot + '/bin:' + '$HOME/' + self._ibmdbClidriverBaseDir + '/bin:$HOME/'
+            #'IBM_DB_HOME': '$IBM_DB_HOME:$HOME/' + self._ibmdbClidriverBaseDir + '/lib',
+            'LD_LIBRARY_PATH': '$LD_LIBRARY_PATH:$HOME/' + self._ibmdbClidriverBaseDir + '/lib',
+            'PATH': '$HOME/' + self._ibmdbClidriverBaseDir + '/bin:$HOME/' 
                     + self._ibmdbClidriverBaseDir + '/adm:$PATH',
         }
-        #self._log.info(env['IBM_DB_HOME'])
         return env
-
-    def _service_commands(self):
-        self._log.info(__file__ + "->service_commands")        
-        return {}
-
-    def _preprocess_commands(self):
-        self._log.info(__file__ + "->preprocess_commands")
-        return ()
 
     def _logMsg(self, logMsg):
         self._log.info(logMsg)
         print logMsg        
 
-    def _install_pecl(self, url, hsh, installDir, fileName=None, strip=False, extract=True):
-        self._service_environment()
-        self._runCmd(os.environ, self._ctx['BUILD_DIR'], ['pecl','install','ibm_db2'])
-        self._runCmd(os.environ, self._ctx['BUILD_DIR'], ['make'])
-        self._runCmd(os.environ, self._ctx['BUILD_DIR'], ['make','install'])
-        
-    def _install_ibm_db(self, url, hsh, installDir, fileName=None, strip=False, extract=True):
-        # hsh for future use
-        if not fileName:
-            fileName = urlparse(url).path.split('/')[-1]
-        fileToInstall = os.path.join(self._ctx['TMPDIR'], fileName)
-        self._runCmd(os.environ, self._ctx['BUILD_DIR'], ['rm', '-rf', fileToInstall])
-        self._log.debug("Installing direct [%s]", url)
-        self._installer._dwn.custom_extension_download(url, url, fileToInstall)
-        self._runCmd(os.environ, self._ctx['BUILD_DIR'], ['mv', fileToInstall, fileToInstall + '.tar.gz'])
-        fileToInstall = fileToInstall + ".tar.gz"     
-
-        if extract:
-            return self._installer._unzipUtil.extract(fileToInstall, installDir, strip)
-        else:
-            shutil.copy(fileToInstall, installDir)
-            return installDir
-        
-    def _extract_include(self, url, hsh, installDir, fileName=None, strip=False, extract=True):
-        # hsh for future use
-        if not fileName:
-            fileName = urlparse(url).path.split('/')[-1]
-        fileToInstall = os.path.join(self._ctx['TMPDIR'], fileName)
-        self._runCmd(os.environ, self._ctx['BUILD_DIR'], ['rm', '-rf', fileToInstall])
-
-        self._log.debug("Installing direct [%s]", url)
-        self._installer._dwn.custom_extension_download(url, url, fileToInstall)
-        self._runCmd(os.environ, self._ctx['BUILD_DIR'],
-                        ['mv',fileToInstall,installDir])
-        
-        
     def _install_direct(self, url, hsh, installDir, fileName=None, strip=False, extract=True):
-        # hsh for future use
         if not fileName:
             fileName = urlparse(url).path.split('/')[-1]
         fileToInstall = os.path.join(self._ctx['TMPDIR'], fileName)
         self._runCmd(os.environ, self._ctx['BUILD_DIR'], ['rm', '-rf', fileToInstall])
-
-        self._log.debug("Installing direct [%s]", url)
         self._installer._dwn.custom_extension_download(url, url, fileToInstall)
 
         if extract:
@@ -177,35 +100,27 @@ class IBMDBInstaller(ExtensionHelper):
     def _runCmd(self, environ, currWorkDir, cmd, displayRunLog=False):
         stringioWriter = StringIO.StringIO()
         try:
-            stream_output(stringioWriter,            #sys.stdout,
-                          ' '.join(cmd),
-                          env=environ,
-                          cwd=currWorkDir,
-                          shell=True)
+            stream_output(stringioWriter, ' '.join(cmd), env=environ, cwd=currWorkDir, shell=True)
+            cmdOutput = stringioWriter.getvalue()
             if displayRunLog:
-                self._log.info(stringioWriter.getvalue())
+                self._logMsg(cmdOutput)
         except:
+            cmdOutput = stringioWriter.getvalue()
             print '-----> Command failed'
-            print stringioWriter.getvalue()
+            print cmdOutput
             raise
+        return cmdOutput
 
-    def _findPhpExtnBaseDir(self):
-        with open(self._phpIniPath, 'rt') as phpIni:
+    def findPhpExtnBaseDir(self):
+        with open(self._phpBuildIniFpath, 'rt') as phpIni:
             for line in phpIni.readlines():
                 if line.startswith('extension_dir'):
                     (key, extnDir) = line.strip().split(' = ')
                     extnBaseDir = os.path.basename(extnDir.strip('"'))
                     return extnBaseDir
 
-    def _parsePhpApi(self):
-        tmp = os.path.basename(self._phpExtnDir)
-        phpApi = tmp.split('-')[-1]
-        phpZts = (tmp.find('non-zts') == -1)
-        return phpApi, phpZts
-
-    def _modifyPhpIni(self):
-        self._log.info('Modifying ' + self._phpIniPath)
-        with open(self._phpIniPath, 'rt') as phpIni:
+    def modifyPhpIni(self):
+        with open(self._phpBuildIniFpath, 'rt') as phpIni:
             lines = phpIni.readlines()
         extns = [line for line in lines if line.startswith('extension=')]
         if len(extns) > 0:
@@ -213,143 +128,45 @@ class IBMDBInstaller(ExtensionHelper):
         else:
             pos = lines.index('#{PHP_EXTENSIONS}\n') + 1
         lines.insert(pos, 'extension=ibm_db2.so\n')
-        #lines.insert(pos, 'extension=pdo_ibm.so\n')
-        #lines.append('\n')
-        self._log.info('Writing ' + self._phpIniPath)
-        with open(self._phpIniPath, 'wt') as phpIni:
+        lines.insert(pos, 'extension=pdo.so\n')
+        lines.insert(pos, 'extension=pdo_ibm.so\n')
+        with open(self._phpBuildIniFpath, 'wt') as phpIni:
             for line in lines:
                 phpIni.write(line)
 
     def install_clidriver(self):
-        #for clidriverpart in ['IBMDBCLIDRIVER1', 'IBMDBCLIDRIVER2']:
-        self._install_direct(
-            self._ctx['IBMDBCLIDRIVER1_DLURL'],
-            None,
-            self._ctx['IBMDBCLIDRIVER_INSTALL_DIR'],
-            self._ctx['IBMDBCLIDRIVER1_DLFILE'],
-            True)
+        self._logMsg('-- Installing IBM DB CLI Drivers -----------------')
+        for clidriverpart in ['ibmdbclidriver1']:
+            if self._ctx[clidriverpart.upper() + '_DLFILE'] != '':
+                self._install_direct(
+                    self._ctx[clidriverpart.upper() + '_DLURL'],
+                    None,
+                    self._ctx['IBMDBCLIDRIVER_INSTALL_DIR'],
+                    self._ctx[clidriverpart.upper() + '_DLFILE'],
+                    True)
 
-        self._logMsg ('Installed IBMDB CLI Drivers to ' + self._ctx['IBMDBCLIDRIVER_INSTALL_DIR'])
-    
-    def _new_php(self):
-        #for clidriverpart in ['IBMDBCLIDRIVER1', 'IBMDBCLIDRIVER2']:
-        os.chdir('/tmp')
-        subprocess.call( ['mkdir','build'])
-        os.chdir('build')
-        subprocess.call( ['mkdir','931e8e8a'])
-        os.chdir('931e8e8a')
-        subprocess.call( ['mkdir','binary-builder'])
-        os.chdir('binary-builder')
-        subprocess.call( ['mkdir','ports'])
-        os.chdir('ports')
-        subprocess.call( ['mkdir','x86_64-linux-gnu'])
-        os.chdir('x86_64-linux-gnu')
-        subprocess.call( ['mkdir','php'])
-        os.chdir('php')
-        subprocess.call( ['mkdir','7.0.14'])
-        os.chdir('7.0.14')
-        
-        phproot = os.path.join(self._ctx['BUILD_DIR'], 'php')
-        self._logMsg ('php root directory')
-        
-        newdir = os.getcwd()
-        self._runCmd(os.environ, self._ctx['BUILD_DIR'],
-                        ['cp -rf',phproot+'/*',newdir])
-        self._logMsg ('copy successful in directory ' + newdir )
-        os.chdir(newdir) 
-        self._extract_include(
-                self._ctx['INCLUDE_DLURL'],
-                None,
-                newdir,
-                self._ctx['INCLUDE_FILE'],
-                True)
-        
-        self._logMsg ('ls in /tmp directory' )
-        subprocess.call(['ls', '-lrt','/tmp'])
-        self._logMsg ('ls in /tmp/build directory' )
-        subprocess.call(['ls', '-lrt','/tmp/build'])
-        self._logMsg ('ls in /tmp/buildpacks directory' )
-        subprocess.call(['ls', '-lrt','/tmp/buildpacks'])
-        
-        #subprocess.call(['gunzip','include.tar.gz'])
-        #subprocess.call(['tar','-xf','include.tar'])
-        
-        '''
-        tar = tarfile.open("include.tar")
-        tar.extractall()
-        tar.close()
-        self._runCmd(os.environ, self._ctx['BUILD_DIR'],
-                        ['gunzip','include.tar.gz'])
-        self._runCmd(os.environ, self._ctx['BUILD_DIR'],
-                        ['tar','-xf','include.tar'])
-        '''
-        subprocess.call(['ls', '-lrt',newdir])
-        return newdir
-        
-    
-    def install_extensions(self):
-        ev = self._service_environment()
-        ospath = os.environ['PATH']
-        osev = os.environ
-        for ibmdbExtn in ['IBM_DB2']: #, 'PDO', 'PDO_IBM']:
-            #extnDownloadDir = os.path.join(self._ctx['DOWNLOAD_DIR'],
-            #                           ibmdbExtn.lower() + '_extn-' + self._ctx[ibmdbExtn + '_VERSION'])
-            extnDownloadDir = os.path.join(self._ctx['DOWNLOAD_DIR'],ibmdbExtn.lower() + '_extn')
-            instDir = os.path.join(extnDownloadDir +'/installDir')
-            self._install_ibm_db(
+        self._compilationEnv['IBM_DB_HOME'] = self._ctx['IBMDBCLIDRIVER_INSTALL_DIR']
+        self._logMsg('-- Installed IBM DB CLI Drivers ------------------')
+
+    def download_extensions(self):
+        self._logMsg('-- Downloading IBM DB Extensions -----------------')
+        for ibmdbExtn in ['IBM_DB']:
+            ibmdbExtnDownloadDir = self._ctx[ibmdbExtn + '_DLDIR']
+            self._install_direct(
                 self._ctx[ibmdbExtn + '_DLURL'],
                 None,
-                extnDownloadDir,
+                ibmdbExtnDownloadDir,
                 self._ctx[ibmdbExtn + '_DLFILE'],
                 True)
-
-            
-            phpRoot = os.path.join(self._ctx['BUILD_DIR'], 'php')
-            phpInstallDir = os.path.join(phpRoot, 'lib', 'php')
-            phpBinDir = os.path.join(phpRoot, 'bin')                       
-            phpExecPath = os.path.join(phpBinDir, 'php')  
-            phpizeExecPath = os.path.join(phpBinDir, 'phpize')
-            phpExtnDir = os.path.join(phpInstallDir, 'extensions')
-            nphp = self._new_php()
-            tmpdir = self._ctx['TMPDIR']            
-            
-            self._logMsg ('extention download directory : ' + extnDownloadDir)
-            curdir = os.getcwd()
-            self._logMsg ('current directory is : ' + curdir)
-            phpRoot = os.path.join(self._ctx['BUILD_DIR'], 'php')
-            self._logMsg(phpRoot)
-
-            self._logMsg('phpbinpath = '+  phpBinDir)
-            self._logMsg('tmpdir = '+  tmpdir)
-            os.chdir(extnDownloadDir)
-            newdir = os.getcwd()
-            self._logMsg ('New directory is : ' + newdir)
-            osev['PATH'] = phpBinDir +':' + os.environ['PATH']
-            self._logMsg('osev path = ' + osev['PATH'])
-            
-            self._logMsg ('ls -l for new Directory')
-            subprocess.call(['ls', '-l'])
-            subprocess.call(['chmod','777', extnDownloadDir])
-            
-            self._logMsg('Execute pecl')
-            #self._runCmd(osev,self._ctx['BUILD_DIR'], ['php', '-i'])
-            
-            self._runCmd(osev,self._ctx['BUILD_DIR'], ['phpize','--version'])
-            #self._runCmd(osev,self._ctx['PHP_PEAR_PHP_BIN'], ['pecl','install','ibm_db2'])
-            #subprocess.call(['phpize'])
-            self._runCmd(osev, phpBinDir,['./configure -with-IBM_DB2='+ self._ctx['IBMDBCLIDRIVER_INSTALL_DIR']] )
-            self._runCmd(osev, extnDownloadDir,['make'])
-            self._runCmd(osev, self._ctx['BUILD_DIR'],['make','install'])
-
-            os.chdir(curdir)
-            self._logMsg ('back to previous directory ' + os.getcwd())         
-            self._logMsg ('Installed ' + ibmdbExtn + ' Extension to ' + self._phpExtnDir)
-            
-
-        #self._modifyPhpIni()
-        #self._log.info(os.getenv('PATH'))
+            self._runCmd(self._compilationEnv, self._ctx['BUILD_DIR'],
+                 ['cp', os.path.join(ibmdbExtnDownloadDir,  self._zendModuleApiNo,'/*'),
+                  self._phpExtnDpath])
+            self._logMsg ('Installed extension ' + ibmdbExtn)
+        self._logMsg('-- Downloaded IBM DB Extensions ------------------')
 
     def cleanup(self):
-        self._runCmd(os.environ, self._ctx['BUILD_DIR'], ['rm', '-rf', self._ctx['DOWNLOAD_DIR']])
+        self._logMsg('-- Some House-keeping ----------------------------')
+        self._runCmd(os.environ, self._ctx['BUILD_DIR'], ['rm', '-rf', self._ctx['COMPILATION_DIR']])
+        self._logMsg('-- House-keeping Done ----------------------------')
 
 IBMDBInstaller.register(__name__)
